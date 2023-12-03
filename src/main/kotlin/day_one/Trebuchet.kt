aaef240bad5edcf4059ab42b060cbe339672bf72
donadelli <davi_donadelli@live.com>
2023-12-03 16:52:45 -0300

    feat: creating structure to read puzzle inputs

---
+++ src/main/kotlin/day_one/Trebuchet.kt
@@ -0,0 +1,34 @@
+package day_one
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    val timeInMillis = measureTimeMillis {
+        firstProblem()
+    }
+
+    println("-------------------------------------")
+    println("(The operation took $timeInMillis ms)")
+}
+
+fun firstProblem(): Int {
+    val stringList = readFile("first_problem_input.txt")
+
+    if (stringList == null) {
+        println("Couldn't find any file to read :(")
+        return -1;
+    }
+
+    /*
+    TODO: Solve algorithm here :)
+     */
+
+    return 0
+}
+
+fun secondProblem(): Int {
+    return 0
+}
+
+fun readFile(fileName: String) =
+    object {}.javaClass.getResourceAsStream(fileName)?.bufferedReader()?.readLines()
